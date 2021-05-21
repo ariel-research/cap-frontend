1a085f1228a86292e28d71d9d1c68b232efac47e
itaysim7 <itaysim7@gmail.com>
2021-05-21 17:25:22 +0300

    timetable

--- src/components/timetable.js
+++ src/components/timetable.js
@@ -18,38 +18,69 @@ function Timetable(props)
             })}
             </div>
 
-            <table border="1" width="50px">
+            <table width= '80%' border="2" cellPadding="0" cellSpacing="0" style={{backgroundColor: 'orange'}}>
                 <thead>
-                    <tr>
-                        <th width="15%">שישי</th>
-                        <th width="15%">חמישי</th>
-                        <th width="15%">רביעי</th>
-                        <th width="15%">שלישי</th>
-                        <th width="15%">שני</th>
-                        <th width="15%">ראשון</th>
-                        <th width="10%">זמן</th>
+                    <tr style={{height: '40px'}}>
+                        <td width="15%">שישי</td>
+                        <td width="15%">חמישי</td>
+                        <td width="15%">רביעי</td>
+                        <td width="15%">שלישי</td>
+                        <td width="15%">שני</td>
+                        <td width="15%">ראשון</td>
+                        <td width="10%">זמן</td>
                     </tr>
                 </thead>
-                <tbody>
-                    {[...Array(15)].map( (e,i) =>{
+                <tbody style={{backgroundColor:'white'}}>
+                {courses && courses.map( (hour,i) => {
+                    if(hour[0].length === 0 && hour[1].length ===0 && hour[2].length ===0 && hour[3].length ===0 && hour[4].length ===0 && hour[5].length ===0)
+                    {
                         return (
-                            <tr key={i}>         
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                { i<2 ? (
-                                    <th>0{i+8}:00</th>
-                                ) : (
-                                    <th>{i+8}:00</th>
-                                )}
+                            <tr key={i} style={{height:'25px', verticalAlign:'top'}}>
+                                {hour && hour.map( (day,j) => {
+                                return (
+                                    <td key={j} style={{ verticalAlign:'top'}}>       
+                                    </td>
+                                )
+                                })}
+                                <td style={{backgroundColor:'orange'}}>{i+8}:00</td>
                             </tr>
-                        );
-                    })}
+                        )
+                    }
+                    return (
+                        <tr key={i} style={{height:'25px', verticalAlign:'top'}}>
+                             {hour && hour.map( (day,j) => {
+                                if(day.length === 0)
+                                {
+                                    return (
+                                    
+                                        <td key={j} style={{ verticalAlign:'top'}}>       
+                                        </td>
+                                    )
+
+                                } 
+                                return (
+                                    <td key={j} style={{ verticalAlign:'top'}}>  
+                                        {day && day.map( (course,k) => {
+                                        return (                                    
+                                            <td key={k} style={{ verticalAlign:'top'}}>
+                                                <h6>
+                                                    {course.course_group}<br/> שם המרצה: {course.lecturer} <br/> שעות: {(course.time_start).substring(0, 5)}-{(course.time_end).substring(0, 5)}
+                                                </h6>                                            </td>
+                                        )
+                                        })}   
+                                    </td>
+                                )
+                                
+                                })}
+                            <td style={{backgroundColor:'orange'}}>{i+8}:00</td>
+                            
+                        </tr>
+
+                    )
+                })}
                 </tbody>
-            
+
+           
             </table>
         </div>
 
