8b7eabc480906cb81ace882bc650a1dab09e96d2
oriyalp <oriya2011@gmail.com>
2023-07-23 15:39:25 +0300

    Add about page

---
+++ src/about.js
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function About() {
+
+    return (
+        <div className="about">
+            <Navbar active="אודות" />
+            <div className="container-fluid mt-2">
+                <h1>קצת על האתר...</h1>
+                <br/>
+                <h2>גרסה 0.0.2 (התשפ"ג)</h2>
+                <p>אוריה אלפרין</p>
+                <h2>גרסה 0.0.1 (התשפ"א)</h2>
+                <p>איתי שמחייב, ליהיא בלפר, יוסף שטיין</p>
+            </div>
+        </div>
+    )
+
+}
+export default About;
