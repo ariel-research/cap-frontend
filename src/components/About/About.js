6c9994cf0232555db01aae88a7ca7b46db25a325
oriyalperin <oriya2011@gmail.com>
2023-07-26 08:05:31 +0000

    style about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,18 +1,38 @@
+import {Releases} from "./Releases"
+import './About.css'
+import {Github, EnvelopeAtFill,} from 'react-bootstrap-icons'
+import { useCookies } from 'react-cookie';
+import logo1 from '../../logo.png';
 import Navbar from "../Navbar/Navbar";
 
 function About() {
-
+    const [token] = useCookies(['mr-token']);
+  
     return (
         <div className="about">
-            <Navbar active="אודות" />
+            {token['mr-token']? <Navbar/> : null }
             <div className="container-fluid mt-2">
-                <h1>קצת על האתר...</h1>
+            <h1>קצת על האתר...</h1>
+                <p>ד"ר אראל דוד סגל-הלוי</p>
                 <br/>
-                <h2>גרסה 0.0.2 (התשפ"ג)</h2>
-                <p>אוריה אלפרין</p>
-                <h2>גרסה 0.0.1 (התשפ"א)</h2>
-                <p>איתי שמחייב, ליהיא בלפר, יוסף שטיין</p>
+              
+                {Releases.map((item, index) => (
+                    <div key={index}>
+                    <h4>גרסא <b>{item.version}</b> {item.year}</h4>
+                    <p>{`מתכנתים: ${item.programmers}`}</p>
+                    </div>
+                ))}
+                <br/>
+                     <ul className="list-unstyled d-flex justify-content-center ">
+                 <li class="ms-3"><a href='https://github.com/ariel-research' className="link-body-emphasis"><Github/></a></li>
+                 <li class="ms-3"><a href='mailto:ariel-research23@gmail.com' className="link-body-emphasis"><EnvelopeAtFill/>ariel-research23@gmail.com</a></li>
+                 </ul>
+
             </div>
+            {!token['mr-token']?
+            <p className="text-center text-muted mt-5 mb-0"><a href="/"
+            className="fw-bold text-body pr"><u>התחברו כאן!</u></a></p> 
+            : null }
         </div>
     )
 
