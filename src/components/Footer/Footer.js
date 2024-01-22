4a49fd5e65576c7c050084678de193208b69d822
Ariel Research: courses <courses@csariel.xyz>
2024-01-22 09:52:20 +0200

    remove ariel

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
                 <div className="nav justify-content-center border-top mb-2">
                     <div className="nav-item"><a href="/about" className="nav-link link-body-emphasis">אודות</a></div>
                 </div>
-                <p className="text-center text-body-secondary">&copy; 2023 אוניברסיטת אריאל</p>
+                <p className="text-center text-body-secondary">&copy; 2023 המעבדה לאלגוריתמים כלכליים </p>
             </footer>
     )
 }
